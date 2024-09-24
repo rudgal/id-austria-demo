fa11db5ea364e9f5a7cc8eb4702f7d9e2e9debe9
rudgal <office@galler.digital>
2024-09-24 08:53:05 +0200

    fix verifyToken test

--- src/util/jwt-util.spec.ts
+++ src/util/jwt-util.spec.ts
@@ -6,8 +6,8 @@ test('verifyToken', async () => {
   const decodedJWT = await verifyToken(exampleResponseOttakringer.id_token, {
     verifyExpiry: false,
   });
-  // console.log('Decoded JWT payload:', decodedJWT);
-  // console.log('keys in JWT payload:', Object.keys(decodedJWT).sort());
+  console.log('Decoded JWT payload:', decodedJWT);
+  console.log('keys in JWT payload:', Object.keys(decodedJWT).sort());
 
   expect(decodedJWT).toBeDefined();
 });
