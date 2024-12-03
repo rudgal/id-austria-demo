4a79bf274d34c2e90fc864c9993f84704a9f1b26
rudgal <office@galler.digital>
2024-12-03 16:17:39 +0100

    merge styles, add tailwind-breakpoints.css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import '@/styles/globals.css';
 import { Inter } from 'next/font/google';
+import { isDevMode } from '@/util/utils';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -11,7 +12,12 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} ${isDevMode() ? 'responsive-breakpoints-debug' : ''}`}>
+        {/*<ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>*/}
+        {/*  {children}*/}
+        {/*</ThemeProvider>*/}
+        {children}
+      </body>
     </html>
   );
 }
