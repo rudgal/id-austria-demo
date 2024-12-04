522cbe38193a46260c0d1694dd28adf3667cd223
rudgal <office@galler.digital>
2024-12-04 18:24:07 +0100

    add little HowTo.mdx

--- src/components/demo-page.tsx
+++ src/components/demo-page.tsx
@@ -1,11 +1,17 @@
-import { DemoSectionLeft } from '@/components/demo-section-left';
-import { DemoSectionRight } from '@/components/demo-section-right';
+import HowTo from '@/mdx/HowTo.mdx';
+import { LoginForm } from '@/components/login-form';
 
 export default function DemoPage() {
   return (
     <>
-      <DemoSectionLeft />
-      <DemoSectionRight />
+      <div className="w-1/2 overflow-y-auto bg-slate-100 p-8 dark:bg-slate-800">
+        <div className="prose prose-sm max-w-none dark:prose-invert">
+          <HowTo />
+        </div>
+      </div>
+      <div className="flex w-1/2 items-center justify-center overflow-y-auto bg-white p-8 dark:bg-slate-950">
+        <LoginForm />
+      </div>
     </>
   );
 }
