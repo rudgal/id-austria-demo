7edcf5cc955d7a637ddba040c62f5fc56724289d
rudgal <office@galler.digital>
2025-07-13 15:44:47 +0200

    update HowTo.mdx

--- src/components/demo-page.tsx
+++ src/components/demo-page.tsx
@@ -4,12 +4,12 @@ import { LoginForm } from '@/components/login-form';
 export default function DemoPage() {
   return (
     <>
-      <div className="w-1/2 overflow-y-auto bg-slate-100 p-8 dark:bg-slate-800">
+      <div className="h-full w-1/2 overflow-y-auto bg-slate-100 p-8 dark:bg-slate-800">
         <div className="prose prose-sm max-w-none dark:prose-invert">
           <HowTo />
         </div>
       </div>
-      <div className="flex w-1/2 items-center justify-center overflow-y-auto bg-white p-8 dark:bg-slate-950">
+      <div className="flex h-full w-1/2 items-center justify-center bg-white p-8 dark:bg-slate-950">
         <LoginForm />
       </div>
     </>
