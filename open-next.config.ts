09b63a225a7690f8c5205f77602e163b3b708a5f
rudgal <office@galler.digital>
2025-07-20 12:29:06 +0200

    cloudflare, add staticAssetsIncrementalCache

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,9 +1,7 @@
-// default open-next.config.ts file created by @opennextjs/cloudflare
-import { defineCloudflareConfig } from "@opennextjs/cloudflare/config";
-// import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
+import { defineCloudflareConfig } from '@opennextjs/cloudflare';
+import staticAssetsIncrementalCache from '@opennextjs/cloudflare/overrides/incremental-cache/static-assets-incremental-cache';
 
 export default defineCloudflareConfig({
-	// Disable R2 incremental cache for local preview
-	// Uncomment the following lines when you have an R2 bucket configured
-	// incrementalCache: r2IncrementalCache,
+  incrementalCache: staticAssetsIncrementalCache,
+  enableCacheInterception: true,
 });
